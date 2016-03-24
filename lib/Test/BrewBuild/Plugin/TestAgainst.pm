826c586ae97731522ddb770f56b99a61f6e746dc
Steve Bertrand <steveb@cpan.org>
2016-03-24 15:43:30 +0000

    initial commit

---
+++ lib/Test/BrewBuild/Plugin/TestAgainst.pm
@@ -0,0 +1,74 @@
+package Test::BrewBuild::Plugin::TestAgainst;
+
+use 5.006;
+use strict;
+use warnings;
+
+package Test::BrewBuild::Plugin::TestAgainst;
+
+our $VERSION = '0.01';
+
+my $module;
+
+sub brewbuild_exec{
+    shift;
+    $module = shift;
+    return _cmd($module);
+}    
+sub _cmd {
+    my $module = shift;
+
+    my @cmd = <DATA>;
+    for (@cmd){
+        s/%\[MODULE\]%/$module/g;
+    }
+
+    return @cmd;
+}    
+
+1;
+
+=head1 NAME
+
+Test::BrewBuild::Plugin::TestAgainst - Test external modules against current
+builds of the one being tested
+
+=head1 SYNOPSIS
+
+    brewbuild --plugin Test::BrewBuild::Plugin::TestAgainst --args Rev::Dep
+
+=head1 DESCRIPTION
+
+This is a plugin for L<Test::BrewBuild>. The plugin sub takes the name of a
+module, and after testing and installing of the revision of the local module, 
+it'll run the test suite of the external module to ensure it passes with the
+current prerequisite codebase.
+
+Useful mainly for testing reverse dependencies of the module you're currently
+working on.
+
+=head1 FUNCTIONS
+
+=head2 brewbuild_exec($module_name);
+
+Takes the name of the module, and returns back the appropriate configuration
+commands to L<Test::Brewbuild>.
+
+=head1 AUTHOR
+
+Steve Bertrand, C<< <steveb at cpan.org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Same as L<Test::BrewBuild>
+
+=cut
+
+__DATA__
+if ($^O eq 'MSWin32'){
+    my $make = -e 'Makefile.PL' ? 'dmake' : 'Build';
+    system "cpanm --installdeps . && cpanm . && cpanm --test-only %[MODULE]%";
+}
+else {
+    system "cpanm --installdeps . && cpanm . && cpanm --test-only %[MODULE]%";
+}
---
+++ t/01-check.t
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use 5.006;
+use strict;
+use warnings;
+
+use Test::BrewBuild::Plugin::TestAgainst;
+use Test::More;
+
+my $mod = 'Test::BrewBuild::Plugin::TestAgainst';
+
+can_ok ($mod, 'brewbuild_exec');
+
+my @ret = $mod->brewbuild_exec('Mock::Sub');
+
+my @data = <DATA>;
+
+is (@data, @ret, "the return from $mod ok");
+
+my $i = 0;
+
+for (@data){
+    s/%\[MODULE\]%/Mock::Sub/g;
+    is ($ret[$i], $_, "$_ line matches baseline");
+    $i++;
+}
+
+done_testing();
+
+__DATA__
+if ($^O eq 'MSWin32'){
+    my $make = -e 'Makefile.PL' ? 'dmake' : 'Build';
+    system "cpanm --installdeps . && cpanm . && cpanm --test-only %[MODULE]%";
+}
+else {
+    system "cpanm --installdeps . && cpanm . && cpanm --test-only %[MODULE]%";
+}
